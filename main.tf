c5eca1023a54dd3a535e39bb4bcbe41bb360d619
Calin Marina <calin.marina@gmail.com>
2020-07-23 02:43:36 +0300

    add Access Origin Identity for CloudFront access to a private S3 bucket

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ locals {
     project = var.domain
     env     = var.env
   }
+  s3_origin_id = "myS3Origin"
 }
 
 # Reuse existing zone to avoid allocation of different name servers after rebuild
@@ -16,30 +17,7 @@ data "aws_route53_zone" "route_zone" {
 #Create domain bucket
 resource "aws_s3_bucket" "domainBucket" {
   bucket = var.domain
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-  }
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${var.domain}/*"
-            ]
-        }
-    ]
-}
-EOF
+  acl    = "private"
 
   tags = local.common_tags
 }
@@ -54,11 +32,18 @@ resource "aws_s3_bucket" "subdomainBucket" {
   tags = local.common_tags
 }
 
+resource "aws_cloudfront_origin_access_identity" "originAccessIdentity" {
+  comment = "Cloud Front Origin Access Identity"
+}
 
-resource "aws_cloudfront_distribution" "cloudFrontDistribution" {
+resource "aws_cloudfront_distribution" "s3Distribution" {
   origin {
     domain_name = aws_s3_bucket.domainBucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.domainBucket.id
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.originAccessIdentity.cloudfront_access_identity_path
+    }
   }
 
   tags = local.common_tags
@@ -69,9 +54,9 @@ resource "aws_cloudfront_distribution" "cloudFrontDistribution" {
   enabled             = true
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.domainBucket.id
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -81,8 +66,12 @@ resource "aws_cloudfront_distribution" "cloudFrontDistribution" {
       }
     }
     compress = true
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
+
 
   restrictions {
     geo_restriction {
@@ -102,8 +91,8 @@ resource "aws_route53_record" "domain_record" {
   type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.cloudFrontDistribution.domain_name
-    zone_id = aws_cloudfront_distribution.cloudFrontDistribution.hosted_zone_id
+    name = aws_cloudfront_distribution.s3Distribution.domain_name
+    zone_id = aws_cloudfront_distribution.s3Distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
